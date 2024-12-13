8cc457d11e8501c05eff0004b09342f9c3de2548
18paulb <bjpaul99@gmail.com>
2024-12-12 21:57:16 -0700

    put back tailwind config

--- journalfrontend/tailwind.config.js
+++ journalfrontend/tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
     darkMode: ["class"],
     content: [
-    "./journalfrontend/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./journalfrontend/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./journalfrontend/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
