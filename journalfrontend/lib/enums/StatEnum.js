23875beb861ae4e73de831462d08007cb25796b0
18paulb <bjpaul99@gmail.com>
2025-02-27 00:09:26 -0700

    reset stats when entry is created

--- journalfrontend/lib/enums/StatEnum.js
+++ journalfrontend/lib/enums/StatEnum.js
@@ -1,7 +1,6 @@
 export const StatEnum = Object.freeze({
-    STREAK: "journalStreak",
-    LONGEST_STREAK: "longestStreak",
-    ENTRY_COUNT: "journalEntryCount",
-    PHOTO_COUNT: "photoCount",
-  });
-  
+  STREAK: 'journalStreak',
+  LONGEST_STREAK: 'longestStreak',
+  ENTRY_COUNT: 'journalEntryCount',
+  PHOTO_COUNT: 'photoCount',
+});
