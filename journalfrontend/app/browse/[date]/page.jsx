3792a8592be7c510ac89b9e115be4c878bfb6c2b
18paulb <bjpaul99@gmail.com>
2025-05-17 00:20:46 -0600

    see yesterday's journals on browse

--- journalfrontend/app/browse/[date]/page.jsx
+++ journalfrontend/app/browse/[date]/page.jsx
@@ -4,9 +4,18 @@ import BrowsePublicJournalsComponent from './browseComponent';
 export default async function BrowsePage({ params }) {
   const date = (await params).date;
 
-  const publicEntries = await getDailyPublicJournalEntries(date);
+  // Get the journal entries for the day before.
+  // Do this because people write at different times so if you see the day before you can see everyone's who wrote.
+  const yesterday = getPreviousDayDate(date);
+  const publicEntries = await getDailyPublicJournalEntries(yesterday);
 
   return (
     <BrowsePublicJournalsComponent publicEntries={publicEntries}></BrowsePublicJournalsComponent>
   );
+}
+
+function getPreviousDayDate(isoDate) {
+  const date = new Date(isoDate);
+  date.setDate(date.getDate() - 1); // go back one day
+  return date.toISOString().split('T')[0]; // return YYYY-MM-DD
 }
