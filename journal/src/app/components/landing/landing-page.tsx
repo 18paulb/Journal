0f61adb143f405eb5226ded3d4928632727cb76f
18paulb <bjpaul99@gmail.com>
2025-06-30 16:25:44 -0600

    add landing age

---
+++ journal/src/app/components/landing/landing-page.tsx
@@ -0,0 +1,17 @@
+import CTASection from './cta-section';
+import FeaturesSection from './features-section';
+import HeroSection from './hero-section';
+import PrivacySection from './privacy-section';
+
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <main className="w-full">
+        <HeroSection />
+        <FeaturesSection />
+        <PrivacySection />
+        <CTASection />
+      </main>
+    </div>
+  );
+}
